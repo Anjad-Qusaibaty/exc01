cab49130db9367c1006eeb1ff7c7dbbca5f57f28
Anjad-Qusaibaty <anjad.qusaibaty@gmail.com>
2021-05-09 22:16:29 +0200

    fixed the style for the Navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,27 +1,27 @@
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import './Navbar.css';
 
 const navbarstyle={
     textDecoration: "none",
     color:"white",
     letterSpacing: "0.5px",
-    position: "relative",
-    marginRight: "3%",
     fontSize: "16px",
     verticalAlign:"middle",
     fontWeight: "bold",
+    position: "relative",
+    margin:"0 10px"
 }
 
 export default function Navbar() {
   return (
     <div className="navBar">
 
-<Link  style ={navbarstyle} to="/">Scoreboard</Link>
-<Link  style ={navbarstyle} to="/likecounter">Like Counter</Link>
-<Link  style ={navbarstyle} to="/awesomeanimals">Awesome Animals</Link>
-<Link  style ={navbarstyle} to="/articleslistapi">Articles (API)</Link>
-<Link  style ={navbarstyle} to="/articleslist">Articles (Local)</Link>
-<Link  style ={navbarstyle} to="/likebutton">Like Button</Link>
+<NavLink  exact activeStyle ={{color:"rgb(0, 255, 204)"}} style={navbarstyle} to="/">Scoreboard</NavLink>
+<NavLink  activeStyle ={{color:"rgb(0, 255, 204)"}} style={navbarstyle} to="/likecounter">Like Counter</NavLink>
+<NavLink  activeStyle ={{color:"rgb(0, 255, 204)"}} style={navbarstyle} to="/awesomeanimals">Awesome Animals</NavLink>
+<NavLink  activeStyle ={{color:"rgb(0, 255, 204)"}} style={navbarstyle} to="/articleslistapi">Articles (API)</NavLink>
+<NavLink  activeStyle ={{color:"rgb(0, 255, 204)"}} style={navbarstyle} to="/articleslist">Articles (Local)</NavLink>
+<NavLink  activeStyle ={{color:"rgb(0, 255, 204)"}} style={navbarstyle} to="/likebutton">Like Button</NavLink>
 
     </div>
   )
